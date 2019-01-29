463e42d7aaa4d23586b7b652d880dff761cbbb22
ksascomm <ksasweb@jhu.edu>
2019-01-29 09:48:30 -0500

    new events single layout file

---
+++ single-ai1ec_event.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all single events
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header();
+?>
+
+<div class="main-container" id="page">
+	<div class="main-grid">
+		<main class="main-content-full-width">
+			<?php
+            while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content-events', '' ); ?>
+			<?php endwhile; ?>
+		</main>
+	<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php
+get_footer();
