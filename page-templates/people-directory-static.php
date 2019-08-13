263cd7ec2d422ea9e7715cb36d67ae7a60ade734
ksascomm <ksasweb@jhu.edu>
2019-08-13 14:56:48 -0400

    orderby slug; display all roles in WP_Query

--- page-templates/people-directory-static.php
+++ page-templates/people-directory-static.php
@@ -7,12 +7,16 @@ get_header(); ?>
 	$theme_option = flagship_sub_get_global_options();
 	$roles = get_terms(
         'role', array(
-			'orderby'       => 'ID',
+			'orderby' 		=> 'slug',
 			'order'         => 'ASC',
 			'hide_empty'    => true,
+	
 		)
-        );
+    );
 	$role_slugs = array();
+	foreach ($roles as $role ) {
+		$role_slugs[] = $role->slug;
+	}
 	$role_classes = implode(' ', $role_slugs);
 	$children = get_pages( array(
 		'child_of' => $post->ID,
@@ -45,22 +49,20 @@ get_header(); ?>
 				<ul class="directory">
 					<?php
                     foreach ($roles as $role ) {
-					$role_slug = $role->slug;
-					$role_name = $role->name;
-					if ($role_slug !== 'graduate' && $role_slug !== 'job-market-candidate' && $role_slug !== 'graduate-student' && $role_slug !== 'research' ) {
-							$people_query = new WP_Query(
-                            array(
-								'post_type' => 'people',
-								'role' => $role_slug,
-								'meta_key' => 'ecpt_people_alpha',
-								'orderby' => 'meta_value',
-								'order' => 'ASC',
-								'posts_per_page' => 250,
-							)
-								);
-											}
-					if ($people_query->have_posts() ) :
-							?>
+						$role_slug = $role->slug;
+						$role_name = $role->name;
+				
+						$people_query = new WP_Query(
+	                    array(
+							'post_type' => 'people',
+							'role' => $role_slug,
+							'meta_key' => 'ecpt_people_alpha',
+							'orderby' => 'meta_value',
+							'order' => 'ASC',
+							'posts_per_page' => 250,
+						) );
+						
+						if ($people_query->have_posts() ) : ?>
 							<li class="person sub-head quicksearch-match <?php echo $role->slug; ?>">
 							<h2 class="black capitalize"><?php echo $role_name; ?>
 							</h2>
