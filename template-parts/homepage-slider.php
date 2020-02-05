3f3979a5d0616db1394a1670a4608ee4d3ddd1be
ksascomm <ksasweb@jhu.edu>
2020-02-05 11:20:38 -0500

    Fix missing alt on sliders

--- template-parts/homepage-slider.php
+++ template-parts/homepage-slider.php
@@ -1,14 +1,6 @@
 <div class="grid-x grid-padding-x">
 	<div class="cell small-12 large-7">
-		<?php
-		// Get the URL of the custom meta image field
-		$get_slider_image_url = get_post_meta(get_the_ID(), 'ecpt_slideimage', true);
-		// Gets the custom post type image id
-		$get_slider_image_id = attachment_url_to_postid($get_slider_image_url);
-		// Get's the alt text from the image
-		$slider_alt = get_post_meta ( $get_slider_image_id, '_wp_attachment_image_alt', true );
-		?>		
-		<img src="<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>" alt="<?php echo $slider_alt;?>" />
+		<img src="<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>" alt="<?php echo get_the_content();?>" />
 	</div>
 	<div class="cell small-12 large-4">
 		<div class="slide-caption">
