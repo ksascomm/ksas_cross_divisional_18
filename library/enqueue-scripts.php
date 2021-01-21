c4eba19f60a7133ec565b28d6fc90489b9b01915
ksascomm <ksascomm@gmail.com>
2021-01-21 09:31:59 -0500

    Set jquery type to minified

--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -78,7 +78,7 @@ endif;
 function defer_parsing_of_js( $url ) {
 	if ( is_user_logged_in() ) return $url; //don't break WP Admin
 	if ( FALSE === strpos( $url, '.js' ) ) return $url;
-	if ( strpos( $url, 'jquery.js' ) ) return $url;
+	if ( strpos( $url, 'jquery.min.js' ) ) return $url;
 	return str_replace( ' src', ' defer src', $url );
 }
 add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
