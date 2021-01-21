79f375f61fcff150cb76e3a6f5eafc7b76dc13e4
ksascomm <ksascomm@gmail.com>
2021-01-21 11:58:48 -0500

    Defer isotope scripts

--- template-parts/script-initiators.php
+++ template-parts/script-initiators.php
@@ -6,21 +6,21 @@
 ?>
 
 <?php if (is_page_template('page-templates/people-directory.php') || is_page_template('page-templates/research-projects.php') ) : ?>
-  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
-  <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js" defer></script>
+  <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js" defer></script>
 <?php endif;?>
 
 <?php if (is_page_template('page-templates/people-directory.php')):?>
-  <script>
+  <script defer>
     jQuery(document).ready(function(e){var t,i;e("#noResult").hide();var o,s,l,n=e("#isotope-list").isotope({itemSelector:".person",layoutMode:"vertical",filter:function(){var o=e(this),s=!t||o.text().match(t),l=!i||o.is(i);return s&&l}}),a=e("#id_search").keyup((o=function(){t=new RegExp(a.val(),"gi"),n.isotope(),n.data("isotope").filteredItems.length?e("#noResult").hide():e("#noResult").show()},function(){l&&clearTimeout(l),setTimeout(function(){o(),l=null},s||100)}));function c(){var t;t=location.hash.match(/filter=([^&]+)/i),(i=t&&t[1])&&(e(".filter-list").find("a.is-checked").removeClass("is-checked"),e(".filter-list").find('[data-filter="'+i+'"]').addClass("is-checked"),n.isotope())}e(".filter-list li a").click(function(e){e.preventDefault()}),e(".filter-list a.button").on("click",function(){if(e(this).hasClass("is-checked"))e(this).removeClass("is-checked"),location.hash="filter=*";else{var t=e(this).attr("data-filter");location.hash="filter="+encodeURIComponent(t)}}),c(),window.onhashchange=c});
 </script>
 <?php endif;?>
 
 <?php if (is_page_template('page-templates/research-projects.php')):?>
-  <script>
+  <script defer>
     jQuery(document).ready(function(e){var t,i;e("#noResult").hide();var o,s,l,n=e("#isotope-list").isotope({itemSelector:".item",layoutMode:"fitRows",filter:function(){var o=e(this),s=!t||o.text().match(t),l=!i||o.is(i);return s&&l}}),a=e("#id_search").keyup((o=function(){t=new RegExp(a.val(),"gi"),n.isotope(),n.data("isotope").filteredItems.length?e("#noResult").hide():e("#noResult").show()},function(){l&&clearTimeout(l),setTimeout(function(){o(),l=null},s||100)}));function c(){var t;t=location.hash.match(/filter=([^&]+)/i),(i=t&&t[1])&&(e(".filter-list").find("a.is-checked").removeClass("is-checked"),e(".filter-list").find('[data-filter="'+i+'"]').addClass("is-checked"),n.isotope())}e(".filter-list li a").click(function(e){e.preventDefault()}),e(".filter-list a.button").on("click",function(){if(e(this).hasClass("is-checked"))e(this).removeClass("is-checked"),location.hash="filter=*";else{var t=e(this).attr("data-filter");location.hash="filter="+encodeURIComponent(t)}}),c(),window.onhashchange=c});
   </script>
-<?php endif;?>  
+<?php endif;?>
 
 <?php if ( is_singular('post') ) : ?>
   <script>
