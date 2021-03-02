16656fcbdc2a7a0a1977aab8f25065080ebad2cf
ksascomm <ksascomm@gmail.com>
2021-03-02 14:31:11 -0500

    Move Disable/Clean Inline Styles function to theme

--- library/cleanup.php
+++ library/cleanup.php
@@ -177,3 +177,33 @@ function deregister_tribe_styles() {
 	    wp_deregister_style( 'tribe-tooltip');
 	}
 }
+
+/** Disable/Clean Inline Styles */
+function clean_post_content( $content ) {
+	// Remove inline styling.
+	// $content = preg_replace( '/(<[^>]+) style=".*?"/i', '$1', $content );.
+	$content = preg_replace( '/(<[span>]+) style=".*?"/i', '$1', $content );
+	$content = preg_replace( '/font-family\:.+?;/i', '', $content );
+	$content = preg_replace( '/color\:.+?;/i', '', $content );
+
+	// Remove font tag.
+	$content = preg_replace( '/<font[^>]+>/', '', $content );
+
+	// Remove empty tags.
+	$post_cleaners = array(
+		'<p></p>'             => '',
+		'<p> </p>'            => '',
+		'<p>&nbsp;</p>'       => '',
+		'<span></span>'       => '',
+		'<span> </span>'      => '',
+		'<span>&nbsp;</span>' => '',
+		'<span>'              => '',
+		'</span>'             => '',
+		'<font>'              => '',
+		'</font>'             => '',
+	);
+	$content       = strtr( $content, $post_cleaners );
+
+	return $content;
+}
+add_filter( 'the_content', 'clean_post_content' );
