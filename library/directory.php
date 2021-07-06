642484eaddbb523500da827b889f7772d029090a
ksascomm <ksascomm@gmail.com>
2021-07-06 11:35:55 -0400

    add `if not empty` conditional

--- library/directory.php
+++ library/directory.php
@@ -5,11 +5,11 @@
 
 function get_the_roles( $post ) {
 	$roles = get_the_terms( $post->ID, 'role' );
-		if ( $roles && ! is_wp_error( $roles ) ) :
+	if ( $roles && ! is_wp_error( $roles ) ) :
 		$role_names = array();
 		foreach ( $roles as $role ) {
 			$role_names[] = $role->slug;
-			}
+		}
 		$role_name = join( ' ', $role_names );
 
 		endif;
@@ -18,28 +18,30 @@ function get_the_roles( $post ) {
 
 add_action( 'template_redirect', 'redirect_empty_bios' );
 function redirect_empty_bios() {
-	if(is_singular('people') ) :
+	if ( is_singular( 'people' ) ) :
 		global $post;
-		$bio = get_post_meta($post->ID, 'ecpt_bio', true);
-		$link = get_post_meta($post->ID, 'ecpt_website', true);
-		if(empty($bio) && isset($link)) {
-		    wp_redirect(esc_url($link), 301);
-		    exit;
+		$bio  = get_post_meta( $post->ID, 'ecpt_bio', true );
+		$link = get_post_meta( $post->ID, 'ecpt_website', true );
+		if ( empty( $bio ) && isset( $link ) ) {
+			wp_redirect( esc_url( $link ), 301 );
+			exit;
 		}
 	endif;
 }
 
 function get_the_directory_filters( $post ) {
 	$directory_filters = get_the_terms( $post->ID, 'filter' );
+	if ( ! empty( $directory_filters ) ) {
 		if ( $directory_filters && ! is_wp_error( $directory_filters ) ) :
-		$directory_filter_names = array();
-		foreach ( $directory_filters as $directory_filter ) {
-			$directory_filter_names[] = $directory_filter->slug;
+			$directory_filter_names = array();
+			foreach ( $directory_filters as $directory_filter ) {
+				$directory_filter_names[] = $directory_filter->slug;
 			}
-		$directory_filter_name = join( ' ', $directory_filter_names );
+			$directory_filter_name = join( ' ', $directory_filter_names );
 
 		endif;
 		return $directory_filter_name;
+	}
 }
 
 function deactivate_on_people_page() {
