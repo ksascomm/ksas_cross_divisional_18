5069464090c63cb26f3686d56218cb06d017b24e
ksascomm <ksasweb@jhu.edu>
2021-10-13 13:51:35 -0400

    Remove hyperlink from heading

--- template-parts/content-news-full.php
+++ template-parts/content-news-full.php
@@ -10,30 +10,30 @@
 
 ?>
 
-<article aria-labelledby="post-<?php the_ID(); ?>" <?php post_class('post-listing news-article'); ?>>
+<article aria-labelledby="post-<?php the_ID(); ?>" <?php post_class( 'post-listing news-article' ); ?>>
 	<header>
 		<h2>
 			<small aria-hidden="true">
 				<?php
-                $categories = get_the_category();
-				$separator = ', ';
-				$output = '';
-					if ( ! empty( $categories ) ) {
+				$categories = get_the_category();
+				$separator  = ', ';
+				$output     = '';
+				if ( ! empty( $categories ) ) {
 					foreach ( $categories as $category ) {
 						$output .= $category->name . $separator;
-					    }
-					echo trim( $output, $separator );
 					}
-				if (has_tag('gallery') ) :
-                ?>
-                , Image Gallery<?php endif; ?>
+					echo trim( $output, $separator );
+				}
+				if ( has_tag( 'gallery' ) ) :
+					?>
+				, Image Gallery<?php endif; ?>
 			</small>
 			<br>
-			<?php if ( get_post_meta($post->ID, 'ecpt_external_link', true) ) : ?>
-				<a href="<?php echo get_post_meta($post->ID, 'ecpt_external_link', true); ?>" target="_blank" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?> <span class="icon-new-tab2" aria-hidden="true"></span>
+			<?php if ( get_post_meta( $post->ID, 'ecpt_external_link', true ) ) : ?>
+				<a href="<?php echo get_post_meta( $post->ID, 'ecpt_external_link', true ); ?>" target="_blank" title="<?php the_title(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?> <span class="icon-new-tab2" aria-hidden="true"></span>
 				</a>
 			<?php else : ?>
-				<a href="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a>
+				<?php the_title(); ?>
 			<?php endif; ?>
 		</h2>
 		<?php ksasacademic_entry_meta(); ?>
@@ -43,16 +43,16 @@
 		<div class="grid-x">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<div class="cell small-12 medium-5 large-3">
-				<?php the_post_thumbnail(array(200,200), array('class' => 'alignleft news-thumb')); ?>
+				<?php the_post_thumbnail( array( 200, 200 ), array( 'class' => 'alignleft news-thumb' ) ); ?>
 			</div>
 			<div class="cell small-12 medium-7 large-9">
 				<?php the_content(); ?>
 			</div>
-		<?php else: ?>
+		<?php else : ?>
 			<div class="cell small-12">
 				<?php the_content(); ?>
 			</div>
-		<?php endif;?>
+		<?php endif; ?>
 		</div>
 	</div>
 </article>
